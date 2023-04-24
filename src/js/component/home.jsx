fa6368d3ffba81041704e421656c97923a91256e
Luis Lugo <124685188+JerryJox@users.noreply.github.com>
2023-04-24 12:36:23 +0000

    upload files

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,9 +5,9 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const[ selectedColor, setSelectedColor] = useState("red");
+	// const[ selectedColor, setSelectedColor] = useState("red");
 	return (
-		<div classNameName="trafficLight">
+		<div className="trafficLight">
 			<div className="light red glow"></div>
 			<div className="light orange"></div>
 			<div className="light green"></div>
